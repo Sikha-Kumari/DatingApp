932c48e437dda3d697b4ec6ad6fe104d8f911af6
Sikha Gupta <Sikha.Gupta@hyland.com>
2021-06-15 20:47:05 +0530

    Basic API Section

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using System.Collections.Generic;
+using API.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+
+        }
+
+
+         [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id) 
+        {
+            var user = await _context.Users.FindAsync(id);
+            return user;
+
+        }
+
+    }
+}
